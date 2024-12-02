c609484d007545059ee94f01792e2bfdf6afa398
georgefdsmith <georgefdsmith@gmail.com>
2024-12-02 00:37:49 +0000

    Finished the Search state for the AI

--- Assets/Scripts/Enemy/States/SearchState.cs
+++ Assets/Scripts/Enemy/States/SearchState.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class SearchState : BaseState
 {
+
+    private float searchTimer;
+    private float moveTimer;
+
     public override void Enter()
     {
-
+        enemy.Agent.SetDestination(enemy.LastKnowPos);
     }
 
     public override void Perform()
     {
+        if (enemy.CanSeePlayer())
+        {
+            stateMachine.ChangeState(new AttackState());
+        }
+        if (enemy.Agent.remainingDistance < enemy.Agent.stoppingDistance)
+        {
+            searchTimer += Time.deltaTime;
+            moveTimer += Time.deltaTime;
 
+            if (moveTimer > Random.Range(1, 3))
+            {
+                enemy.Agent.SetDestination(enemy.transform.position + (Random.insideUnitSphere * 10));
+                moveTimer = 0;
+            }
+
+            if (searchTimer > 5)
+            {
+                stateMachine.ChangeState(new PatrolState());
+            }
+        }
     }
 
     public override void Exit()

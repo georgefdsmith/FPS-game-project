e448b8d7917467dc90429c447c84da15836211bc
georgefdsmith <georgefdsmith@gmail.com>
2024-12-09 02:24:00 +0000

    Finally got a good shooting particle effect working

---
+++ Assets/Scripts/Muzzle.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+{
+    [Tooltip("Socket at the tip of the Muzzle. Commonly used as a firing point.")]
+    [SerializeField]
+    private Transform socket;
+
+    [Header("Particles")]
+
+    [Tooltip("Firing Particles.")]
+    [SerializeField]
+    private GameObject prefabFlashParticles;
+
+    [Tooltip("Number of particles to emit when firing.")]
+    [SerializeField]
+    private int flashParticlesCount = 5;
+
+    [Header("Flash Light")]
+
+    [Tooltip("Muzzle Flash Prefab. A small light we use when firing.")]
+    [SerializeField]
+    private GameObject prefabFlashLight;
+
+    [Tooltip("Time that the light flashed stays active. After this time, it is disabled.")]
+    [SerializeField]
+    private float flashLightDuration;
+
+    [Tooltip("Local offset applied to the light.")]
+    [SerializeField]
+    private Vector3 flashLightOffset;
+
+    /// <summary>
+    /// Instantiated Particle System.
+    /// </summary>
+    private ParticleSystem particles;
+    /// <summary>
+    /// Instantiated light.
+    /// </summary>
+    private Light flashLight;
+
+    private void Awake()
+    {
+        //Null Check.
+        if (prefabFlashParticles != null)
+        {
+            //Instantiate Particles.
+            GameObject spawnedParticlesPrefab = Instantiate(prefabFlashParticles, socket);
+            //Reset the position.
+            spawnedParticlesPrefab.transform.localPosition = default;
+            //Reset the rotation.
+            spawnedParticlesPrefab.transform.localEulerAngles = default;
+
+            //Get Reference.
+            particles = spawnedParticlesPrefab.GetComponent<ParticleSystem>();
+        }
+
+        //Null Check.
+        if (prefabFlashLight)
+        {
+            //Instantiate.
+            GameObject spawnedFlashLightPrefab = Instantiate(prefabFlashLight, socket);
+            //Reset the position.
+            spawnedFlashLightPrefab.transform.localPosition = flashLightOffset;
+            //Reset the rotation.
+            spawnedFlashLightPrefab.transform.localEulerAngles = default;
+
+            //Get reference.
+            flashLight = spawnedFlashLightPrefab.GetComponent<Light>();
+            //Disable.
+            flashLight.enabled = false;
+        }
+    }
+
+    public void Effect()
+    {
+        //Try to play the fire particles from the muzzle!
+        if (particles != null)
+            particles.Emit(flashParticlesCount);
+
+        //Make sure that we have a light to flash!
+        if (flashLight != null)
+        {
+            //Enable the light.
+            flashLight.enabled = true;
+            //Disable the light after a few seconds.
+            StartCoroutine(nameof(DisableLight));
+        }
+    }
+
+    private IEnumerator DisableLight()
+    {
+        //Wait.
+        yield return new WaitForSeconds(flashLightDuration);
+        //Disable.
+        flashLight.enabled = false;
+    }
+}
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -7,17 +7,25 @@ public class PlayerShoot : MonoBehaviour
     public Transform gunBarrel;
     public GameObject shootEffect;
 
+    public Muzzle muzzle;
+
     public void Shoot()
     {
         Transform gunbarrel = gunBarrel;
 
+        // Create the bullet
         GameObject bullet = GameObject.Instantiate(Resources.Load("Prefabs/Bullet") as GameObject, gunbarrel.position, gunBarrel.rotation);
 
         bullet.GetComponent<Rigidbody>().velocity = gunBarrel.forward * 50;
 
-        if (shootEffect != null )
+        if (shootEffect != null)
         {
             GameObject effect = Instantiate(shootEffect, gunBarrel.position, gunBarrel.rotation);
+        }
+
+        if (muzzle != null)
+        {
+            muzzle.Effect();
         }
     }
 }
